0cff4ea8bfe840ae101579e98f9cc3943627a9f3
mochi8k <y.hkh.185@gmail.com>
2016-11-04 12:38:16 +0900

    handle chat

--- server.go
+++ server.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func mainHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my page")
 }
 
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, _ := template.ParseFiles("templates/chat.tpl")
+	tpl.Execute(w, "hello")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/chat", chatHandler)
+
+	fmt.Println("activated the web server on port 8080")
+	http.ListenAndServe(":8080", nil)
+
 }
